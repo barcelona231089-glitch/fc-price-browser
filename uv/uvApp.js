13d2fbb1799cce4c4d6a3e1cd42cdfcec2d13fd9
barcelona231089-glitch <barcelona231089@gmail.com>
2026-09-21 05:43:12 +0200

    Keep UV API version compatibility

--- uv/uvApp.js
+++ uv/uvApp.js
@@ -17,7 +17,7 @@ import { buildReportedOutcomeScore } from './src/outcomeLearning.js';
 import { attachLocalFutbinFc27 } from './src/futbinLocalFc27.js';
 
 export const uvRouter = express.Router();
-const UV_VERSION = '2.11.1';
+const UV_VERSION = '2.11.0';
 
 async function getUvMarketContext(platform, liveCards = []) {
   const realRows = await loadRealMarketRegimeRows(platform).catch(() => []);
